3f48af31050a7b5774f21912bf2bbf78be465008
geovanex24 <geovanex24@hotmail.com>
2024-09-23 15:49:55 -0600

    feat: add password encryption for user registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 const createUser = async (req, res) => {
@@ -13,6 +14,11 @@ const createUser = async (req, res) => {
     }
 
     user = new User(req.body);
+
+    //Encriptar contraseña
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
+
     await user.save();
 
     res.status(201).json({
